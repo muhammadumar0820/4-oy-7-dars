5c092dd12c3f58378af62898576ebc2afc5e8156
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2023-12-21 18:08:46 +0300

    masalalar bajarildi

---
+++ index.js
@@ -0,0 +1,39 @@
+//1-masala
+//let arr = [2, 4, 5, 7, 8];
+//function countArr(arg){
+//    let counter = 0;
+//    for (let i  = 0; i < arg.lengh; i++){
+//        if (arg[i] % 2 == 1){
+//            counter=counter+1;
+//        }
+//    }
+//}
+//countArr(arr);
+
+//2-masala
+//let arr = [21, 14, 35, 27, 88];
+//function sumArr(arg){
+//    let sum = 0;
+//    for (let i  = 0; i < arg.lengh; i++){
+//        if (arg[i] % 3 == 0 && arg[i] % 7 ==0){
+//            sum=sum*i;
+//        }
+//    }
+//}
+//countArr(arr);
+
+//6-masala
+//let arr = [11, 4, 22, 17, 89];
+//function countArr(arg){
+//    let sum = 0;
+//    let counter = 0;
+//    for (let i  = 0; i < arg.lengh; i++){
+//        if (arg[i] % arr[i] == 0){
+//            sum=sum+1;
+//        }
+//        if (sum==2){
+//            counter=counter+1
+//}
+//    }
+//}
+//countArr(arr);
